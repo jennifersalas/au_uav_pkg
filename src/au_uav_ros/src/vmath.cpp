8a92eba4546e6af7097cf7539cb13e084ac3d75a
doodlemissy <jennysalas@me.com>
2016-06-07 16:09:22 -0500

    Modified vMath to factor in angle of elevation as an optional parameter defaulted to 90 degrees. Should not affect existing architecture.
    Added cross product.

--- src/au_uav_ros/src/vmath.cpp
+++ src/au_uav_ros/src/vmath.cpp
@@ -8,14 +8,16 @@ are related to implementation, not usage.
 #include "au_uav_ros/standardFuncs.h" /* for PI */
 
 /* Constructor - specify magnitude and direction of the force vector */
-au_uav_ros::mathVector::mathVector(double m, double d) :
-	magnitude(m), direction(d) {}
+au_uav_ros::mathVector::mathVector(double m, double d, double a) :
+	magnitude(m), direction(d), eAngle(a) {}
 
 /* Copy constructor */
 au_uav_ros::mathVector::mathVector(const au_uav_ros::mathVector& mV) {
 	this->direction = mV.direction;
 	this->magnitude = mV.magnitude;
+	this->eAngle = mV.eAngle;
 }
+
 
 /* Accessor methods: Allow the client to access the magnitude and direction of the math vector */
 double au_uav_ros::mathVector::getDirection(void) const {
@@ -24,6 +26,10 @@ double au_uav_ros::mathVector::getDirection(void) const {
 
 double au_uav_ros::mathVector::getMagnitude(void) const {
 	return magnitude;
+}
+
+double au_uav_ros::mathVector::getElevationAngle(void) const {
+    return eAngle;
 }
 
 /* Modifier methods: Allow the client to modify the magnitude and direction of the math vector */
@@ -35,24 +41,73 @@ void au_uav_ros::mathVector::setMagnitude(double m) {
 	this->magnitude = m;
 }
 
+void au_uav_ros::mathVector::setElevationAngle(double a) {
+    this->eAngle = a;
+}
+
 /*
 Vector-based addition, subtract, and multiplication operator overloading.
 */
 
-/* Dot produt. */ 
+/* Vector Dot Product. */ 
 double au_uav_ros::mathVector::dotProduct(const au_uav_ros::mathVector& mV){
-	/* Ax = mag * cos(degree), Ay = mag * sin(degree) */
-	double Ax = this->magnitude * cos(this->direction * PI / 180.0);
-	double Ay = this->magnitude * sin(this->direction * PI / 180.0);
+    double Ax = 0.0, Ay = 0.0, Az = 0.0, Bx = 0.0, By = 0.0, Bz = 0.0;
+    double Rx = 0.0, Ry = 0.0, Rz = 0.0, R = 0.0, Theta = 0.0, Phi = 0.0;
 
-	/* Bx = mag * cos(degree), By = mag * sin(degree) */
-	double Bx = mV.magnitude * cos(mV.direction * PI / 180.0);
-	double By = mV.magnitude * sin(mV.direction * PI / 180.0);
+ 	// Ax = mag * cos(theta) * sin (phi), Ay = mag * sin(theta) * sin (phi), Az = mag * cos (phi)
+    Ax = this->magnitude * cos(this->direction * PI / 180.0)*sin(this->eAngle * PI /180.0);
+	Ay = this->magnitude * sin(this->direction * PI / 180.0) * sin(this->eAngle * PI /180.0);
+    Az = this->magnitude * cos (this->eAngle * PI / 180.0);
+                                                                        
+	// Bx = mag * cos(theta) * sin (phi), By = mag * sin(theta) * sin (phi),  Bz = mag * cos (phi)
+	Bx = mV.magnitude * cos(mV.direction * PI / 180.0) * sin(mV.eAngle * PI / 180);
+	By = mV.magnitude * sin(mV.direction * PI / 180.0)* sin(mV.eAngle * PI / 180);
+	Bz = mV.magnitude * cos (mV.eAngle * PI / 180.0);
 
-	return Ax*Bx+Ay*By;
+	return Ax*Bx+Ay*By+Az*Bz;
 
 }
 
+/* Vector Cross Product */
+
+const au_uav_ros::mathVector au_uav_ros::mathVector::crossProduct(const au_uav_ros::mathVector& mV) {
+    double Ax = 0.0, Ay = 0.0, Az = 0.0, Bx = 0.0, By = 0.0, Bz = 0.0;
+    double Rx = 0.0, Ry = 0.0, Rz = 0.0, R = 0.0, Theta = 0.0, Phi = 0.0;
+
+ 	// Ax = mag * cos(theta) * sin (phi), Ay = mag * sin(theta) * sin (phi), Az = mag * cos (phi)
+	Ax = this->magnitude * cos(this->direction * PI / 180.0) * sin(this->eAngle * PI /180.0);
+	Ay = this->magnitude * sin(this->direction * PI / 180.0) * sin(this->eAngle * PI /180.0);
+    Az = this->magnitude * cos (this->eAngle * PI / 180.0);
+                                                                        
+	// Bx = mag * cos(theta) * sin (phi), By = mag * sin(theta) * sin (phi),  Bz = mag * cos (phi)
+	Bx = mV.magnitude * cos(mV.direction * PI / 180.0) * sin(mV.eAngle * PI / 180);
+	By = mV.magnitude * sin(mV.direction * PI / 180.0) * sin(mV.eAngle * PI / 180);
+	Bz = mV.magnitude * cos (mV.eAngle * PI / 180.0);
+ 
+ 	//Perform cross product
+ 	Rx =   Ay*Bz - By*Az ;
+	Ry = -(Ax*Bz - Az*Bx);
+ 	Rz =   Ax*By - Ay*Bz ;
+ 
+	/* R = (Rx^2 + Ry^2 +Rz^2)^(1/2) */
+	R = sqrt(pow(Rx, 2) + pow(Ry, 2) + pow(Rz, 2));
+
+	/* Theta = arctan2(Ry, Rx) */
+	Theta = atan2(Ry , Rx) * 180.0 / PI;
+ 
+ 	/* Phi = arccos ( Rz / Rxy ) */
+ 	Phi = acos ( Rz / sqrt(pow(Rx, 2) + pow(Ry, 2)) ) * 180 / PI;
+
+	/* Assign new values */
+	this->magnitude = R;
+	this->direction = Theta;
+ 	this->eAngle = Phi;
+
+	/* Return result */
+	return *this;
+}
+
+/* Overload Vector Addition */
 const au_uav_ros::mathVector au_uav_ros::mathVector::operator+(const au_uav_ros::mathVector& mV) const {
 	mathVector newVector(*this); /* Copy vector */
 	newVector += mV; /* Use += to assign */
@@ -60,35 +115,43 @@ const au_uav_ros::mathVector au_uav_ros::mathVector::operator+(const au_uav_ros:
 }
 
 au_uav_ros::mathVector& au_uav_ros::mathVector::operator+=(const au_uav_ros::mathVector& mV) {
-	double Ax = 0.0, Ay = 0.0, Bx = 0.0, By = 0.0;
-	double Rx = 0.0, Ry = 0.0, R = 0.0, Theta = 0.0;
+	double Ax = 0.0, Ay = 0.0, Az = 0.0, Bx = 0.0, By = 0.0, Bz = 0.0;
+	double Rx = 0.0, Ry = 0.0, Rz = 0.0, R = 0.0, Theta = 0.0, Phi = 0.0;
 
-	/* Ax = mag * cos(degree), Ay = mag * sin(degree) */
-	Ax = this->magnitude * cos(this->direction * PI / 180.0);
-	Ay = this->magnitude * sin(this->direction * PI / 180.0);
+ 	// Ax = mag * cos(theta) * sin (phi), Ay = mag * sin(theta) * sin (phi), Az = mag * cos (phi)
+	Ax = this->magnitude * cos(this->direction * PI / 180.0) * sin (this->eAngle * PI /180.0);
+	Ay = this->magnitude * sin(this->direction * PI / 180.0) * sin (this->eAngle * PI / 180.0);
+ 	Az = this->magnitude * cos (this->eAngle * PI /  180.0 );
 
-	/* Bx = mag * cos(degree), By = mag * sin(degree) */
-	Bx = mV.magnitude * cos(mV.direction * PI / 180.0);
-	By = mV.magnitude * sin(mV.direction * PI / 180.0);
+	// Bx = mag * cos(theta) * sin (phi), By = mag * sin(theta) * sin (phi),  Bz = mag * cos (phi)
+	Bx = mV.magnitude * cos(mV.direction * PI / 180.0) * sin (mV.eAngle * PI / 180.0);
+	By = mV.magnitude * sin(mV.direction * PI / 180.0) * sin (mV.eAngle * PI / 180.0);
+ 	Bz = mV.magnitude * cos (mV.eAngle * PI / 180.0);
 
 	/* Perform addition */
 	Rx = Ax + Bx;
 	Ry = Ay + By;
+ 	Rz = Az + Bz;
 
-	/* R = (Rx^2 + Ry^2)^(1/2) */
-	R = sqrt(pow(Rx, 2) + pow(Ry, 2));
+	/* R = (Rx^2 + Ry^2 +Rz^2)^(1/2) */
+	R = sqrt(pow(Rx, 2) + pow(Ry, 2) + pow(Rz, 2));
 
 	/* Theta = arctan2(Ry, Rx) */
 	Theta = atan2(Ry , Rx) * 180.0 / PI;
+ 
+ 	/* Phi = arccos ( Rz / Rxy ) */
+ 	Phi = acos ( Rz / sqrt(pow(Rx, 2) + pow(Ry, 2)) ) * 180 / PI;
 
 	/* Assign new values */
 	this->magnitude = R;
 	this->direction = Theta;
+ 	this->eAngle = Phi;
 
 	/* Return resulting vector */
 	return *this;
 }
 
+/* Overload Vector Subtraction */
 const au_uav_ros::mathVector au_uav_ros::mathVector::operator-(const au_uav_ros::mathVector& mV) const {
 	mathVector newVector(*this);
 	newVector -= mV;
@@ -96,30 +159,37 @@ const au_uav_ros::mathVector au_uav_ros::mathVector::operator-(const au_uav_ros:
 }
 
 au_uav_ros::mathVector& au_uav_ros::mathVector::operator-=(const au_uav_ros::mathVector& mV) {
-	double Ax = 0.0, Ay = 0.0, Bx = 0.0, By = 0.0;
-	double Rx = 0.0, Ry = 0.0, R = 0.0, Theta = 0.0;
-
-	/* Ax = mag * cos(degree), Ay = mag * sin(degree) */
-	Ax = this->magnitude * cos(this->direction * PI / 180.0);
-	Ay = this->magnitude * sin(this->direction * PI / 180.0);
-
-	/* -Bx = mag * cos(-degree), -By = mag * sin(-degree) */
-	Bx = mV.magnitude * cos((mV.direction + 180.0) * PI / 180.0);
-	By = mV.magnitude * sin((mV.direction + 180.0) * PI / 180.0);
+	double Ax = 0.0, Ay = 0.0, Az = 0.0, Bx = 0.0, By = 0.0, Bz = 0.0;
+	double Rx = 0.0, Ry = 0.0, Rz = 0.0, R = 0.0, Theta = 0.0, Phi = 0.0;
+    
+    // Ax = mag * cos(theta) * sin (phi), Ay = mag * sin(theta) * sin (phi), Az = mag * cos (phi)
+	Ax = this->magnitude * cos(this->direction * PI / 180.0) * sin(this->eAngle * PI / 180.0);
+	Ay = this->magnitude * sin(this->direction * PI / 180.0) * sin(this->eAngle * PI / 180.0);
+ 	Bz = this->magnitude * cos (this->eAngle * PI / 180.0);
+    
+    // Bx = mag * cos(theta) * sin (phi), By = mag * sin(theta) * sin (phi),  Bz = mag * cos (phi)
+	Bx = mV.magnitude * cos((mV.direction) * PI / 180.0) * sin(this->eAngle * PI / 180.0);
+	By = mV.magnitude * sin((mV.direction) * PI / 180.0) * sin(this->eAngle * PI / 180.0);
+ 	Bz = mV.magnitude * cos (mV.eAngle * PI / 180.0);
 
 	/* Perform addition with negative vector */
-	Rx = Ax + Bx;
-	Ry = Ay + By;
+	Rx = Ax - Bx;
+	Ry = Ay - By;
+ 	Rz = Az - Bz;
 
-	/* R = (Rx^2 + Ry^2)^(1/2) */
-	R = sqrt(pow(Rx, 2) + pow(Ry, 2));
+	/* R = (Rx^2 + Ry^2 + Rz^2)^(1/2) */
+	R = sqrt(pow(Rx, 2) + pow(Ry, 2) + pow(Rz, 2));
 
 	/* Theta = arctan2(Ry, Rx) */
 	Theta = atan2(Ry , Rx) * 180.0 / PI;
+ 
+ 	/* Phi = arccos ( Rz / Rxy ) */
+ 	Phi = acos ( Rz / sqrt(pow(Rx, 2) + pow(Ry, 2)) ) * 180 / PI;
 
 	/* Assign new values */
 	this->magnitude = R;
 	this->direction = Theta;
+ 	this->eAngle = Phi;
 
 	/* Return resulting vector */
 	return *this;
@@ -129,6 +199,7 @@ au_uav_ros::mathVector& au_uav_ros::mathVector::operator-=(const au_uav_ros::mat
 Double-based operator overloading for multiplication and division.
 */
 
+// Scalar Multiplication
 const au_uav_ros::mathVector au_uav_ros::mathVector::operator*(double val) const {
 	mathVector newVector(*this);
 	newVector *= val;
@@ -143,6 +214,7 @@ au_uav_ros::mathVector& au_uav_ros::mathVector::operator*=(double val) {
 	return *this;
 }
 
+//Scalar Division
 const au_uav_ros::mathVector au_uav_ros::mathVector::operator/(double val) const {
 	mathVector newVector(*this);
 	newVector /= val;
@@ -176,6 +248,7 @@ const au_uav_ros::mathVector operator/(double val, const au_uav_ros::mathVector&
 au_uav_ros::mathVector& au_uav_ros::mathVector::operator=(const au_uav_ros::mathVector& mV){
 	this->magnitude = mV.magnitude;
 	this->direction = mV.direction;
+ 	this->eAngle = mV.eAngle;
 	return *this;
 }
 
